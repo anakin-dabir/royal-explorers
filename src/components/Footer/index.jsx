39616f12c02412dc57df13c8c6cc24660a8293b4
anakin-dabir <anakindabir@gmail.com>
2025-04-01 13:37:14 +0500

    Upd: Added footer places

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,33 @@
 import { data } from "@/data";
 import { Facebook, Twitter, Linkedin } from "lucide-react";
+import places from "@/data/popular-destinations.json";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className="bg-footer text-white">
       <div className="container mx-auto max-w-7xl px-4 py-10 flex flex-col md:flex-row items-start justify-between gap-8">
         {/* Left Section */}
-        <div>
-          <h2 className="text-xl font-semibold text-white flex items-center gap-2 uppercase">
-            <span className="text-blue-500">Royal</span> Explorers
-          </h2>
-          <p className="mt-4 text-sm">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-xl font-semibold text-white flex items-center gap-2 uppercase">
+              <span className="text-blue-500">Royal</span> Explorers
+            </h2>
+            <p className="text-sm">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          </div>
+
+          <div className="flex items-center gap-4">
+            {places?.map((place, index) => {
+              return (
+                <div key={index} className="size-20 relative">
+                  <Image alt={place.region} src={place.image} fill className="object-cover" />
+                  <div className="absolute inset-0 z-10 flex bg-black/60 items-center text-center p-1 justify-center">
+                    <div className="text-xs font-bold">{place.region}</div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
 
         {/* Right Section */}
